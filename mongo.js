d76f86ccf6bddcaa667b22f90f4773ffee41af95
Arnav Goel <2828.arnav@gmail.com>
2025-07-24 10:37:15 +0000

    completed part 3

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // const password = process.argv[2]
 
-const url = "mongodb+srv://arnavgg458:qlCQEnDeGdJ6fvES@cluster0.fknuy9s.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=cluster0"
+const url = 'mongodb+srv://arnavgg458:qlCQEnDeGdJ6fvES@cluster0.fknuy9s.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=cluster0'
 
 mongoose.set('strictQuery',false)
 
@@ -10,31 +10,31 @@ mongoose.connect(url)
 
 const peopleSchema = new mongoose.Schema({
   name: String,
-  number: String, 
+  number: String,
 })
 
 const People = mongoose.model('People', peopleSchema)
 
 if (process.argv.length <3) {
-    People.find({}).then(result => {
-        console.log("phonebook:")
-        result.forEach(person=>{
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  People.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 
 }
 else{
-    const people = new People({
+  const people = new People({
     name: process.argv[2],
     number: process.argv[3],
-    })
+  })
 
-    people.save().then(result => {
+  people.save().then(() => {
     console.log(`added ${process.argv[2]} number ${process.argv[3]} to the database`)
     mongoose.connection.close()
-    })
+  })
 }
 
 // Note.find({}).then(result => {
